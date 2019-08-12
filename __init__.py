98b9a44a0a2cb201e58c3a8d591a9f2d9b255090
Lynchon83 <45634961+Lynchon83@users.noreply.github.com>
2019-08-12 21:49:52 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ bl_info = {
     "name" : "Lynchon Tools 2.80",
     "author" : "Lynchon",
     "description" : "",
-    "version" : (1,0,0), 
+    "version" : (1,0,2), 
     "blender" : (2, 81, 0),
     "location" : "",
     "warning" : "",
